4d8cd925e09dbcd6811c4ea15085fcf71ef83fc1
alan <35761977+thealanle@users.noreply.github.com>
2019-09-22 12:20:15 -0700

    Add name storage

--- js/main.js
+++ js/main.js
@@ -2,4 +2,33 @@ function openSettings() {
    document.getElementById("settings").classList.toggle("settings-open");
 }
 
+function saveName() {
+    localStorage.setItem('receivedName', userName);
+}
+
+function changeName() {
+   userName = document.getElementById("name-input").value;
+   saveName();
+}
+
+function getGreeting() {
+   document.getElementById("greeting").innerHTML  = `Hello, ${userName}. Enjoy your day!`;
+}
+
+
 document.getElementById("settings-button").addEventListener('click', openSettings)
+
+var userName = localStorage.getItem('receivedName');
+
+if (userName == null) {
+   userName = "friend";
+}
+
+document.getElementById("name-form").addEventListener('submit', function(e) {
+   e.preventDefault()
+   changeName();
+   location.reload();
+
+});
+
+getGreeting()
