c5febca7a03d923f62556919c1b8a5ddff454ead
alan <35761977+thealanle@users.noreply.github.com>
2019-09-22 16:42:43 -0700

    Remove debug statements and new tab functionality

--- js/popup.js
+++ js/popup.js
@@ -11,26 +11,20 @@ function callExpasy(dna_sequence) {
     var url = "https://web.expasy.org/cgi-bin/translate/dna2aa.cgi"
     var params = "dna_sequence=" + dna_sequence + "&output_format=fasta";
 
-    // alert(url + params)
-
     xhr.open("POST", url);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.send(params);
 
-
     xhr.onload = function() {
         if (xhr.status = 200) {
-            // alert("Response: 200. Contents: " + xhr.responseText)
             result = xhr.responseText.replace(/\n>/g, "\n\n>").replace(/\n/g, "<br>")
             document.getElementById("form-output").innerHTML = `<h2>Output:</h2>${result}`;
-
         } else {
             document.getElementById("form-output").innerHTML = `unable to get translation`;
             alert("failed");
         }
     }
 }
-
 
 document.getElementById("nucleotide-form").addEventListener('submit', function(e) {
    e.preventDefault();
