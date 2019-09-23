ef39551c20d3ec04d9829c5bd376304863f430e5
alan <35761977+thealanle@users.noreply.github.com>
2019-09-22 23:08:32 -0700

    Implement context menu link to translate selected text

--- js/background.js
+++ js/background.js
@@ -1,1 +1,11 @@
-var item = chrome.contextMenus.create(type="normal")
+function genericOnClick(e) {
+    window.open("https://web.expasy.org/cgi-bin/translate/dna2aa.cgi?dna_sequence=" + encodeURI(e.selectionText), "_blank")
+}
+
+var args = {
+    "title": "Translate with ExPASy",
+    "contexts": ["selection"],
+    "onclick": genericOnClick
+}
+
+var menuItem = chrome.contextMenus.create(args);
